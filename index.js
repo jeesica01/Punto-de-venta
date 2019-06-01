6f74996cea198e33144155ce7eb66a3b4e1a09f3
Jessica Solis <jeesica01>
2019-06-01 14:24:07 -0500

    Proyecto Punto de Venta Final

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ $(function(){
 	$('[data-toggle="tooltip"]').tooltip()
 })
 
+function ajax(){
+			var ajaxRes = new XMLHttpRequest()
+			ajaxRes.onreadystatechange = function(){
+				if (ajaxRes.readyState == 4 && ajaxRes.status == 200) {
+					document.getElementById("preguntas").innerHTML=ajaxRes.responseText
+				}
+			}
+			ajaxRes.open("GET","Preguntas.html",true)
+			//ajaxRes.open("GET","texto.txt",true)
+			ajaxRes.send()
+		}
